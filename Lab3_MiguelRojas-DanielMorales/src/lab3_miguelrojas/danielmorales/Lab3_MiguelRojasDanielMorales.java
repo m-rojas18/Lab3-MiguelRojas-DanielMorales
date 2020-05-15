1ec33cc8c1f0d4798ba11466799d7a11bc9ad9ab
Miguel Rojas <m_rojas18@unitec.edu>
2020-05-15 14:20:59 -0600

    Crear Usuario

--- Lab3_MiguelRojas-DanielMorales/src/lab3_miguelrojas/danielmorales/Lab3_MiguelRojasDanielMorales.java
+++ Lab3_MiguelRojas-DanielMorales/src/lab3_miguelrojas/danielmorales/Lab3_MiguelRojasDanielMorales.java
@@ -1,24 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3_miguelrojas.danielmorales;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
-/**
- *
- * @author miguel
- */
+
 public class Lab3_MiguelRojasDanielMorales {
 
-    /**
-     * @param args the command line arguments
-     */
-    static Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    static Scanner input = new Scanner(System.in);
+    static ArrayList lista_usuarios = new ArrayList();
+
+    public static void main(String[] args) throws ParseException {
+        
+        ArrayList<Cliente> lista_clientes = new ArrayList();
+        
         char resp = 's';
         while (resp == 's' || resp == 'S') {
             System.out.println("Bienvenido al Shopping Mall de muy muy lejos.");
@@ -26,14 +24,62 @@ public class Lab3_MiguelRojasDanielMorales {
             System.out.println("2. Log in");
             System.out.println("3. Salir");
             int op = input.nextInt();
+            input = new Scanner(System.in);
+            System.out.println();
             switch (op) {
                 case 1:
+                    System.out.print("--------------------\n"
+                            + "Crear Usuario\n"
+                            + "Ingrese su id: ");
+                    //Id
+                    String id = input.next();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Usuario
+                    System.out.print("Ingrese un usuario: ");
+                    String usuario = input.nextLine();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Contraseña: 
+                    System.out.print("Ingrese una contraseña: ");
+                    String password = input.next();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Correo
+                    System.out.print("Ingrese su correo electronico: ");
+                    String correo = input.next();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Nombre Completo
+                    System.out.print("Ingrese su nombre completo: ");
+                    String nombre_completo = input.nextLine();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Fecha de Nacimiento
+                    System.out.print("Ingrese su fecha de nacimiento(dd/mm/yyyy): ");
+                    String fecha = input.next();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Formatear fecha
+                    SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
+                    Date fecha_nacimiento = sd.parse(fecha);
+                    //Cantidad de Dinero
+                    System.out.print("Ingrese cantidad de dinero: ");
+                    int cantidad_dinero = input.nextInt();
+                    input = new Scanner(System.in);
+                    System.out.println();
+                    //Agregar a lista de clientes
+                    lista_clientes.add(new Cliente(cantidad_dinero, id, usuario, password, correo, nombre_completo, fecha_nacimiento));
                     
+                    System.out.println("Se creo la cuenta exitosamente!!\n");
                     break;
                 case 2:
                     break;
+                case 3:
+                    resp = 'n';
+                    break;
                 default:
-                    System.exit(0);
+                    System.out.println("Ingrese una opcion valida.\n");
             }
         }
     }
